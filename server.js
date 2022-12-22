dcec0be2f7b38623f91f4d58e23822ce888ad9f1
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-12-23 00:18:16 +0530

    initial

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/UserRoutes");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://localhost:27017/netflix", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/user", userRoutes);
+
+// serving the frontend
+app.use(express.static(path.join(_dirname, "./client/build")));
+
+app.get("*", function(_,res) {
+  res.sendFile(
+    path,join(_dirname, "./netflix-ui/build", "index.html"),
+    function(err) {
+      res.statusCode(500).send(err);
+    }
+  );
+});
+//
+
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
